6e90751815a95e85376865b35c3013fcac8779d7
dunix241 <dunix241@gmail.com>
2024-08-06 15:09:36 +0700

    feat(neotree): neotree smart toggle

--- LazyVim/config/lua/config/keymaps/nvimtree.lua
+++ LazyVim/config/lua/config/keymaps/nvimtree.lua
@@ -3,15 +3,50 @@ local map = vim.keymap.set
 local error, nvim_tree = pcall(require, "nvim-tree")
 
 if not error then
+  map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Explorer" })
   map("", "<C-\\>", require("utils.open_file_in_vertical_split"))
   map("", "<C-->", require("utils.open_file_in_horizontal_split"))
   map("", "<C-|>", require("utils.open_file_in_vertical_split"))
   map("", "<C-_>", require("utils.open_file_in_horizontal_split"))
-  map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Explorer" })
   map("", "<F5>", function()
     local api = require("nvim-tree.api")
     if api.tree.focus then
       api.tree.reload()
     end
   end)
+end
+
+local tree_focused = function(timeout)
+  return vim.api.nvim_buf_get_option(0, "filetype") == "neo-tree"
+end
+
+local get_state = function(source_name, winid)
+  if source_name == nil then
+    local success
+    success, source_name = pcall(vim.api.nvim_buf_get_var, 0, "neo_tree_source")
+    if not success then
+      return nil
+    end
+  end
+  local state = require("neo-tree.sources.manager").get_state(source_name, nil, winid)
+  if not state.tree then
+    return nil
+  end
+  if not state._ready then
+    return nil
+  end
+  return state
+end
+
+if error then
+  map("n", "<leader>e", function()
+    if not get_state() or not tree_focused() then
+      require("neo-tree.command").execute({ action = "focus", dir = LazyVim.root() })
+      return
+    end
+    require("neo-tree.command").execute({ action = "close" })
+  end, { desc = "Explorer" })
+  map("n", "<leader>ge", function()
+    require("neo-tree.command").execute({ source = "git_status", toggle = true })
+  end, { desc = "Git Explorer" })
 end
