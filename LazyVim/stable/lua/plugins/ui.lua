146a76704ef31bea9a4a890431a9897d51a40238
dunix241 <dunix241@gmail.com>
2024-11-12 16:07:02 +0700

    feat: enable toggle maximize (window)

--- LazyVim/stable/lua/plugins/ui.lua
+++ LazyVim/stable/lua/plugins/ui.lua
@@ -7,12 +7,23 @@ return {
     opts = {},
     keys = function()
       LazyVim.toggle.map("<leader>Sm", {
-        name = "maximizing window",
+        name = "maximizing window (global)",
         get = function()
           return not require("focus.modules.utils").is_disabled()
         end,
         set = function(state)
           require("focus.modules.functions").focus_toggle()
+        end,
+      })
+
+      LazyVim.toggle.map("<leader>SM", {
+        name = "maximizing window (window)",
+        get = function()
+          return not vim.w.focus_disable
+        end,
+        set = function(state)
+          vim.w.focus_disable = not state
+          Focus.resize()
         end,
       })
     end,
