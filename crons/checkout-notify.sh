51c08dee991a1fb5ae1cde58e03de4a3cfb0e93b
dunix241 <dunix241@gmail.com>
2024-12-10 06:05:52 +0700

    feat(crons): update crons

---
+++ crons/checkout-notify.sh
@@ -0,0 +1,1 @@
+DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u)/bus notify-send "Check out" "Don't forget to check out" --icon /home/dunix/data/crons/assets/shutdown.png --app-name "Cautious!!" --urgency critical
