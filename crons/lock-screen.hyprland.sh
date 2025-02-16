6065753010df9f457734de3900ec1551bd3728a9
datvnt <datvnt@vntt.com.vn>
2025-02-17 06:22:21 +0700

    feat(crons): handle lock screen dynamically

--- crons/lock-screen.hyprland.sh
+++
@@ -1,2 +0,0 @@
-# WAYLAND_DISPLAY=wayland-1 XDG_RUNTIME_DIR=/run/user/1001 HYPRLAND_INSTANCE_SIGNATURE=0bd541f2fd902dbfa04c3ea2ccf679395e316887_1735428739_1670333966 hyprctl dispatch lockscreen
-hyprctl --instance 0 dispatch exec swaylock
--- crons/lock-screen.sh
+++ crons/lock-screen.sh
@@ -1,1 +1,5 @@
-DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u)/bus qdbus6 org.freedesktop.ScreenSaver /ScreenSaver Lock
+if [ "$XDG_CURRENT_DESKTOP" != "Hyprland" ]; then
+  DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u)/bus qdbus6 org.freedesktop.ScreenSaver /ScreenSaver Lock
+else
+  hyprctl --instance 0 dispatch exec swaylock
+fi
