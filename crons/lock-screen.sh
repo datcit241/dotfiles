541c9c13a8a3ce123933a2495cadf563ebe992e8
datvnt <datvnt@vntt.com.vn>
2025-03-09 20:06:46 +0700

    feat(cron): update crons

--- crons/lock-screen.sh
+++ crons/lock-screen.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [ "$XDG_CURRENT_DESKTOP" != "Hyprland" ]; then
   DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u)/bus qdbus6 org.freedesktop.ScreenSaver /ScreenSaver Lock
 else
